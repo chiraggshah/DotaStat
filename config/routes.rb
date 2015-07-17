5c95158b26d02005c1076bccabcc3661fc4809ad
Chirag Shah <shahchirag1709@gmail.com>
2015-07-17 16:41:22 +0530

    - added routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'matches#index'
 
+  match '/auth/:provider/callback', to: 'sessions#create', via: :all
+  delete '/logout', to: 'sessions#destroy', as: :logout
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
