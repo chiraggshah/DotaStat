d6e35283383a0b1e605f3f62c46672ee5f94d3a6
Chirag Shah <shahchirag1709@gmail.com>
2015-07-17 17:03:53 +0530

    - added controller action to login and logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+
+  def self.from_omniauth(auth)
+    info = auth['info']
+    #convert from 64bit to 32bit
+    # While authenticating via Steam, the user’s 64-bit uID will be returned.
+    # However, when listing Dota 2 matches, the user’s 32-bit ids are used, instead.
+    user = find_or_initialize_by( uid: (auth['uid'].to_i - 76561197960265728).to_s )
+    user.nickname = info['nickname']
+    user.avatar_url = info['image']
+    user.profile_url = info['urls']['Profile']
+    user.save!
+    user
+  end
+
 end
